da04d82d9d67dbe52c1a33701a2f99c0b718dcda
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2022-05-06 16:10:19 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// reach out to the jsonplaceholder api - specifically the /posts route (ajax request)
+// I want you to paint that data to the dom. Append to the cardList
+
+// add event listeners
+
+// add ids to each
+
+// add functionality to where if an item is clicked on, it creates a new dom element, and appends THAT to the itemDisplay
+
+
+// Jsonplacehoder url ----> https://jsonplaceholder.typicode.com/
